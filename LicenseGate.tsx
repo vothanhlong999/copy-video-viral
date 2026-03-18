7b10f0fcc0a88568431d0cb09553b9123393cf4a
vothanhlong999 <137386678+vothanhlong999@users.noreply.github.com>
2026-03-19 01:39:37 +0700

    Update script URL in LicenseGate.tsx

--- LicenseGate.tsx
+++ LicenseGate.tsx
@@ -17,7 +17,7 @@ export const LicenseGate = ({ children }: { children: React.ReactNode }) => {
     setLoading(true);
     
     // THAY URL NÀY BẰNG URL APPS SCRIPT CỦA BẠN (Ảnh 2 bạn gửi)
-    const scriptUrl = "https://script.google.com/macros/s/AKfycbxqDg8A4uPhNlXRqV_lhOGlEKeElSQS2LF4f6HvGXzZ_S1FiS8_UY1BKSfE195nnv6BPw/exec"; 
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbxKB2i_iFbU01uDteJ04pegospZeIp34NCMG9pxK5_Z0aj9XnCA0OuMqKz-2yxwPStz-w/exec"; 
     const deviceId = window.navigator.userAgent + window.screen.width;
 
     try {
